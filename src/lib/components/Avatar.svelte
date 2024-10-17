f19977cdc5958a66a5ca9168e98bae912cbf5e89
zach <128881293+zatzby@users.noreply.github.com>
2024-10-16 22:10:29 -0700

    Initial commit

---
+++ src/lib/components/Avatar.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import { Avatar, Dropdown, DropdownHeader, DropdownItem, DropdownDivider, Tooltip } from 'flowbite-svelte';
+</script>
+  
+<Avatar data-name="Jese Leos" rounded src="/images/profile-picture-1.webp" />
+<Tooltip>Jese Leos</Tooltip>
+<Avatar data-name="Robert Gouth" rounded src="/images/profile-picture-2.webp" />
+<Tooltip>Robert Gouth</Tooltip>
+<Avatar data-name="Bonnie Green" rounded src="/images/profile-picture-3.webp" />
+<Tooltip>Bonnie Green</Tooltip>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    // import Quote from '$lib/components/Quote.svelte';
+</script>
+
+<div class="text-center">
+
+    <h1 class="mt-10 mb-20 text-4xl">Nostr and Photography are great in their own right, <br />
+        but together they are even more powerful.</h1>
+
+    <p class="text-2xl mb-20">This website is a work in progress, and is still finding its identity.
+        <br/> It is one one thing to have a domain, but another to build on it... so here I am looking for you!
+         <br/> I need help to build it and am taking all ideas. </p>
+         
+    <p class="text-2xl mb-40">Current ideas include using this site as: 
+        <li class="mt-5">a relay dedicated to photography</li>
+        <li class="mt-5">a marketplace for photographers to sell their photography</li>
+        <li class="mt-5">a photography forum for professionals and amateurs</li>
+        <li class="mt-5">a photography microapp like instagram back when it was photos only</li>
+        <li class="mt-5">Your idea here...</li>
+    </p>
+
+</div>
+
+<div class="text-center text-2xl mb-40">Feel free to DM on Nostr at <br /> npub10fu0hlkx3s4n4dsgfu0cpqephga4afr4qtzpz9vsyqf7vj88v2yqdp8vp4 <br /> or contribute at the Github repository <a href="sample.com">here</a></div>
+
+
