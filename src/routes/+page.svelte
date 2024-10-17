4a82c491aefa6fa062a0313e4ba1050fbc06182a
zach <128881293+zatzby@users.noreply.github.com>
2024-10-16 22:36:34 -0700

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,7 +11,7 @@
         <br/> It is one one thing to have a domain, but another to build on it... so here I am looking for you!
          <br/> I need help to build it and am taking all ideas. </p>
          
-    <p class="text-2xl mb-40">Current ideas include using this site as: 
+    <p class="text-2xl mb-10">Current ideas include using this site as: 
         <li class="mt-5">a relay dedicated to photography</li>
         <li class="mt-5">a marketplace for photographers to sell their photography</li>
         <li class="mt-5">a photography forum for professionals and amateurs</li>
@@ -21,4 +21,4 @@
 
 </div>
 
-<div class="text-center text-2xl mb-40">Feel free to DM on Nostr at <br /> npub10fu0hlkx3s4n4dsgfu0cpqephga4afr4qtzpz9vsyqf7vj88v2yqdp8vp4 <br /> or contribute at the Github repository <a href="sample.com">here</a></div>
+<div class="text-center text-2xl mt-20 mb-40">Feel free to DM on Nostr at <br /> npub10fu0hlkx3s4n4dsgfu0cpqephga4afr4qtzpz9vsyqf7vj88v2yqdp8vp4 <br /> or contribute at the Github repository <a href="sample.com">here</a></div>
